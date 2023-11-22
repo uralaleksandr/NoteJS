7d9b37c1e06b50fa2b618caf8b3af8fb2e108315
uralaleksandr <aleksandrural@hotmail.com>
2023-11-22 16:35:19 +0400

    feat: removed routing

--- app.js
+++ app.js
@@ -17,14 +17,8 @@ app.use(expressLayouts);
 app.set("layout", "./layouts/main");
 app.set("view engine", "ejs");
 
-app.get("/", function (req, res) {
-  const locals = {
-    title: "NoteJS",
-    description: "Node Js Notes App",
-  };
-
-  res.render("index", locals);
-});
+//Routes
+app.use("/", require("./server/routes/index"));
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
