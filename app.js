aaf45197ad0f6580d6ffc786311f709bdaafbb9a
uralaleksandr <aleksandrural@hotmail.com>
2023-11-22 14:44:37 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+
+const app = express();
+const port = 5000 || process.env.port;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Static files
+app.use(express.static("public"));
+
+//Templating Engine
+app.use(expressLayouts);
+app.set("layout", "./layouts/main");
+app.set("view engine", "ejs");
+
+app.get("/", function (req, res) {
+  res.render("index");
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
